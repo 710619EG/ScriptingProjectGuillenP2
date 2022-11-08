95b306458f4443180239ec96b861e3f54f3ade3a
710619EG <116373777+710619EG@users.noreply.github.com>
2022-11-08 10:00:16 -0800

    Created a new scene "ScopeAndAccessModifiers"  and two new C# scripts"ScopeAndAccessModifiers" and "AnotherClass". Also created an Empty Game Object in scene ScopeAndAccessModifiers".
    I made a new scene and named it ScopeAndAccessModifiers then created two new C# scripts called ScopeAndAccessModifiers and AnotherClass then added them as components to an Empty Game Object.

---
+++ ScriptingProjectsGuillen/Assets/ScopeAndAccessModifiers.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeAndAccessModifiers : MonoBehaviour
+{
+    //Creates a variable and sets a value to that variable that any class/void/script can use or modify.
+    public int alpha = 5;
+    //Creates a variable and sets a value to that variable. But, only any void in the class the variable is in can use or modify that variable
+    private int beta = 0;
+    private int gtamma = 5;
+
+    private AnotherClass myOtherClass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alpha = 29;
+        myOtherClass = new AnotherClass();
+        myOtherClass.FruitMachine(alpha, myOtherClass.apples);
+    }
+    void Example (int pens, int crayons)
+    {
+        int answer;
+        answer = pens * crayons * alpha;
+        Debug.Log(answer);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Alpha is set to: " + alpha);
+    }
+}
