ee98e540370a240e0c51f780da8637bdee1e3512
710619EG <116373777+710619EG@users.noreply.github.com>
2023-03-14 12:20:57 -0700

    changed textures

--- ScriptingProjectsGuillen/Assets/Scripts/AxisRawExample.cs
+++ ScriptingProjectsGuillen/Assets/Scripts/AxisRawExample.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AxisExample : MonoBehaviour
+public class AxisRawExample : MonoBehaviour
 {
     public float range;
-
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
+        float h = Input.GetAxisRaw("Horizontal");
         float xPos = h * range;
         transform.position = new Vector3(xPos, 0.5f, -0.5f);
         Debug.Log("Value Returned: " + h.ToString("F2"));
